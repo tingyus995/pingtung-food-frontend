4ec835921dc05b061fb356f8dbfbce8e7e62de73
TingYuYan <tingyus995@gmail.com>
2019-12-08 12:41:48 +0800

    added CartHistory and Profile

---
+++ src/views/CartHistory.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="add-food container-fluid">
+    <div class="card" v-for="(order,index) in orders" :key="index">
+      <div class="card-header">
+         <b-badge class="float-left">已完成</b-badge>
+        <font-awesome-icon icon="store-alt"></font-awesome-icon>{{ order.shop}}
+      </div>
+
+      <div class="card-body" >
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item text-center" v-if="!order.items.length">沒有訂單</li>
+
+          <li class="list-group-item text-left" v-for="item in order.items" :key="item.id">
+            {{ item.name}}
+            <span class="badge badge-success">{{ item.amount }}</span>
+            <div class="float-right right-price">
+              <div class="price">{{ item.amount }} x {{item.price }}</div>
+
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="card-footer">
+        <font-awesome-icon icon="coins"></font-awesome-icon>
+        共{{ totalPrice(order) }}元
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cart",
+  components: {},
+  data() {
+    return {
+      orders: [
+        {
+          shop: "捲捲泰式料理",
+          items: [
+            {
+              id: 1,
+              name: "番茄炒飯",
+              amount: 1,
+              price: 150
+            },
+            {
+              id: 2,
+              name: "鮮奶茶",
+              amount: 2,
+              price: 60
+            }
+          ]
+        }
+      ],
+
+      tag: "",
+      tags: []
+    };
+  },
+  methods: {
+    deleteItem(order, i) {
+      //console.log("debug");
+      //console.log(id);
+      order.items = order.items.filter(item => {
+        return item != i;
+      });
+      //this.orders.splice(1,1);
+      //console.log(this.orders);
+    },
+        totalPrice(order) {
+      console.log(order);
+      let price = 0;
+
+      order.items.forEach(item => {
+        price += item.price * item.amount;
+      });
+
+      return price;
+    }
+  },
+  computed: {
+
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.right-price > * {
+  display: inline-block;
+}
+.right-price .price {
+  padding-right: 10px;
+}
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="add-food container-fluid">
+    
+    <h1>{{ user.name}} </h1>
+    <p>訂餐次數：15</p>
+    <p>點餐未領取次數：0</p>
+
+    <b-button variant="primary">檢視我買過的餐點</b-button>
+    <div>
+      <b-button v-b-toggle.collapse-1 variant="primary">變更密碼</b-button>
+      <b-collapse id="collapse-1" class="mt-2">
+        <b-card>
+          <label for="inputPassword" class>請輸入舊密碼</label>
+          <input type="password" id="inputPassword" class="form-control" placeholder="密碼" required />
+
+          <label for="inputPassword" class>請輸入新密碼</label>
+          <input type="password" id="inputPassword" class="form-control" placeholder="密碼" required />
+
+          <label for="inputPassword" class>再輸入一次新密碼</label>
+          <input type="password" id="inputPassword" class="form-control" placeholder="密碼" required />
+          <button class="btn btn-danger" type="submit">變更密碼</button>
+        </b-card>
+      </b-collapse>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueTagsInput from "@johmun/vue-tags-input";
+export default {
+  name: "foods",
+  components: {
+    VueTagsInput
+  },
+  data() {
+    return {
+      user: {
+        name: "TingYu Yan",
+        email: "cbe107059@nptu.edu.tw"
+      }
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.food-img {
+  max-height: 200px;
+  overflow: hidden;
+}
+.food-list .price {
+  font-weight: bold;
+}
+
+.vue-tags-input {
+  max-width: none !important;
+  width: 100%;
+}
+.ti-input {
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
+    -webkit-box-shadow 0.15s ease-in-out;
+}
+
+.ti-input:hover {
+  color: #495057;
+  background-color: #fff;
+  border-color: #80bdff;
+  outline: 0;
+  -webkit-box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+  box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+}
+</style>
